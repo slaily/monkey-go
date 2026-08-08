b42cf290cbe99204273a041c6da6c770762dd5cd
slaily <slavov.iliyan96@gmail.com>
2026-08-08 16:03:32 +0300

    Add REPL package with initial implementation

---
+++ repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/slaily/monkey-go/lexer"
+	"github.com/slaily/monkey-go/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for t := l.NextToken(); t.Type != token.EOF; t = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", t)
+		}
+	}
+}
