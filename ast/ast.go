cf78a47def582d8e578c87f20daf854c0811783b
slaily <slavov.iliyan96@gmail.com>
2026-08-09 08:53:11 +0300

    Added AST for let statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/slaily/monkey-go/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -13,6 +15,23 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 type Program struct {
 	Statements []Statement
